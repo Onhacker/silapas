25c94456f7249f31ce9cdc2dd8b85c0e16f3f27f
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-09-08 03:03:29 +0800

    asd

--- application/controllers/Public_cron.php
+++ application/controllers/Public_cron.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Public_cron extends CI_Controller
+class Public_cron extends Onhacker_Controller
 {
     public function __construct()
     {
