79aa4fa6882e1f24a305f2857a693b06dbd0e797
Onhacker <onhacker@MacBook-Pro-Onhacker.local>
2025-09-20 00:33:42 +0800

    asd

--- application/modules/booking/views/booking_detail.php
+++ application/modules/booking/views/booking_detail.php
@@ -207,6 +207,46 @@ body.noblur-backdrop #app {
     <div class="row">
       <!-- KIRI -->
       <div class="col-md-7">
+
+        <div class="card-box">
+          <div class="media mb-3">
+            <img class="d-flex mr-3 rounded-circle avatar-lg" src="assets/images/users/user-8.jpg" alt="Generic placeholder image">
+            <div class="media-body">
+              <h4 class="mt-0 mb-1">Jade M. Walker</h4>
+              <p class="text-muted">Branch manager</p>
+              <p class="text-muted"><i class="mdi mdi-office-building"></i> Vine Corporation</p>
+
+              <a href="javascript: void(0);" class="btn- btn-xs btn-info">Send Email</a>
+              <a href="javascript: void(0);" class="btn- btn-xs btn-secondary">Call</a>
+              <a href="javascript: void(0);" class="btn- btn-xs btn-secondary">Edit</a>
+            </div>
+          </div>
+
+          <h5 class="mb-3 mt-4 text-uppercase bg-light p-2"><i class="mdi mdi-account-circle mr-1"></i> Personal Information</h5>
+          <div class="">
+            <h4 class="font-13 text-muted text-uppercase">About Me :</h4>
+            <p class="mb-3">
+              Hi I'm Johnathn Deo,has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type.
+            </p>
+
+            <h4 class="font-13 text-muted text-uppercase mb-1">Date of Birth :</h4>
+            <p class="mb-3"> March 23, 1984 (34 Years)</p>
+
+            <h4 class="font-13 text-muted text-uppercase mb-1">Company :</h4>
+            <p class="mb-3">Vine Corporation</p>
+
+            <h4 class="font-13 text-muted text-uppercase mb-1">Added :</h4>
+            <p class="mb-3"> April 22, 2016</p>
+
+            <h4 class="font-13 text-muted text-uppercase mb-1">Updated :</h4>
+            <p class="mb-0"> Dec 13, 2017</p>
+
+          </div>
+
+        </div>
+
+
         <dl class="mb-0">
           <div class="kv-row row no-gutters align-items-center">
             <dt class="col-sm-4 kv-label">🔑 Kode</dt>
